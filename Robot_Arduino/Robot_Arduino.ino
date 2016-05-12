3799df0573ff1b1d880dd27c8ab750d6fc54a7d6
Robert Belter <robbotorigami@gmail.com>
2016-05-11 18:19:49 -0600

    arduino code now works

--- Robot_Arduino/Robot_Arduino.ino
+++ Robot_Arduino/Robot_Arduino.ino
@@ -2,6 +2,8 @@
 
 #include <std_msgs/UInt16MultiArray.h>
 #include <std_msgs/Int16MultiArray.h>
+
+#include <std_msgs/Float32MultiArray.h>
 #include <std_msgs/Int8MultiArray.h>
 #define midvalue 187
 #define range 20
@@ -20,7 +22,7 @@ ros::NodeHandle nh;
 unsigned int oldEncoderValues[8];
 unsigned long oldTime;
 std_msgs::UInt16MultiArray encPos;
-std_msgs::Int16MultiArray encSpeed;
+std_msgs::Float32MultiArray encSpeed;
 
 void motor_cb(const std_msgs::Int8MultiArray& message){
   if(message.data_length < 8) return;
@@ -48,8 +50,25 @@ void setup() {
   
   nh.initNode();
   nh.advertise(encoderPos);
-  nh.advertise(encoderSpeed);
-  nh.subscribe(sub);
+  
+  encPos.layout.dim = (std_msgs::MultiArrayDimension *) malloc(sizeof(std_msgs::MultiArrayDimension) * 2);
+  encPos.layout.dim_length = 1;
+  encPos.layout.dim[0].label = "Label";
+  encPos.layout.dim[0].size = 8;
+  encPos.layout.dim[0].stride = 1*8;
+  encPos.layout.data_offset = 0;
+  encPos.data = (uint16_t *)malloc(sizeof(uint16_t)*8);
+  
+  encSpeed.layout.dim = (std_msgs::MultiArrayDimension *) malloc(sizeof(std_msgs::MultiArrayDimension) * 2);
+  encSpeed.layout.dim_length = 1;
+  encSpeed.layout.dim[0].label = "Label";
+  encSpeed.layout.dim[0].size = 8;
+  encSpeed.layout.dim[0].stride = 1*8;
+  encSpeed.layout.data_offset = 0;
+  encSpeed.data = (float *)malloc(sizeof(float)*8);
+  
+  //nh.advertise(encoderSpeed);
+  //nh.subscribe(sub);
 }
 
 
@@ -62,8 +81,8 @@ void runAllMotors(unsigned short values[8]){
 
 void loop() {
   for(int i = 0; i < 8; i++){
-    encPos.data[i] = map(analogRead(encoderPins[i]), 0, 1024, 0, 360);
-    encSpeed.data[i] = (analogRead(encoderPins[i]) - oldEncoderValues[i]) * 1000 / (millis() - oldTime);
+    //encPos.data[i] = map(analogRead(encoderPins[i]), 0, 1024, 0, 360);
+    //encSpeed.data[i] = 1000 * (float)(analogRead(encoderPins[i]) - oldEncoderValues[i]) / (millis() - oldTime);
     oldEncoderValues[i] = analogRead(encoderPins[i]);
   }
   encPos.data_length = 8;
@@ -71,6 +90,7 @@ void loop() {
   oldTime = millis();
   
   encoderPos.publish( &encPos);
-  encoderSpeed.publish(&encSpeed);
+  //encoderSpeed.publish(&encSpeed);
   nh.spinOnce();  
+  delay(100);
 }
